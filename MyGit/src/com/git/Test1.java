5946dc527c3eabc5d8549ab6a2823387fd670124
atul123git <atulchandane95@gmail.com>
2022-12-22 10:02:51 +0530

    Program is added

---
+++ MyGit/src/com/git/Test1.java
@@ -0,0 +1,14 @@
+package com.git;
+
+import java.util.ArrayList;
+
+public class Test1 {
+public static void main(String args[]) {
+	ArrayList <Integer> al=new ArrayList<Integer>();
+	al.add(10);
+	al.add(40);
+	al.add(50);
+	al.add(60);
+	System.out.println(al);
+}
+}
